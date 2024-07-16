267e833e3467adeeb9e93a5351abf0331ea28214
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-16 11:52:32 +0200

    feat: add state

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 import './App.css'
 
 const message =[
@@ -6,21 +6,21 @@ const message =[
 ]
 
 function App() {
-  const step =1;
+  const [step,setStep ] = useState(1)
   
   function handlePrevious (){
-    alert("previous")
+    setStep(step-1)
   }
 function handleNext (){
-  alert("Next")
+  setStep(step+1)
 }
   
   return (
     <div className='text-center w-2/4 mx-auto bg-slate-200 rounded-2xl mt-28 shadow-md'>
       <div className='grid grid-rows-3 grid-cols-3 gap-4 p-5'>
-        <div className='active ml-32'>1</div>
-        <div className=' not-active ml-28'>2</div>
-        <div className='not-active ml-20'>3</div>
+        <div className={`${step >= 1 ? "active" :"not-active"} ml-32`}>1</div>
+        <div className={` ${step >= 2 ? "active" :"not-active"}  ml-28`}>2</div>
+        <div className={`${step >= 3 ? "active" :"not-active"} ml-20`}>3</div>
         <div className=' col-start-1  col-end-7 '>
           <p className='text-lg font-bold text-indigo-800 mt-5 mb-5'> step {step } : "{message[step-1]}"</p>
         </div>
