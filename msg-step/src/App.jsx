5a3107a3392f18e5de5d247d913047ad58ede2a9
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-16 09:54:14 +0200

    feat : add style inline

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -5,9 +5,18 @@ function App() {
   
 
   return (
-    <>
-     hello
-    </>
+    <div className='text-center w-2/4 mx-auto bg-slate-200 rounded-2xl mt-28'>
+      <div className='grid grid-rows-3 grid-cols-3 gap-4'>
+        <div className='bg-indigo-600 w-16 h-16 rounded-full text-white pt-4 font-bold text-lg ml-32'>1</div>
+        <div className=''>2</div>
+        <div className=''>3</div>
+        <div className=' col-start-1  col-end-7 '>4</div>
+        <div className=''>5</div>
+        <div className=''>6</div>
+
+      </div>
+    
+    </div>
   )
 }
 
