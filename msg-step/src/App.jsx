6d4bf952e0066cec49b9a35ab54df322a436bb0d
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-16 11:06:05 +0200

    refactor : write function handlePrevious and handleNext

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -7,7 +7,14 @@ const message =[
 
 function App() {
   const step =1;
-
+  
+  function handlePrevious (){
+    alert("previous")
+  }
+function handleNext (){
+  alert("Next")
+}
+  
   return (
     <div className='text-center w-2/4 mx-auto bg-slate-200 rounded-2xl mt-28 shadow-md'>
       <div className='grid grid-rows-3 grid-cols-3 gap-4 p-5'>
@@ -18,10 +25,10 @@ function App() {
           <p className='text-lg font-bold text-indigo-800 mt-5 mb-5'> step {step } : "{message[step-1]}"</p>
         </div>
         <div className='custom-btn ml-80'>
-          <button>Previous</button>
+          <button onClick ={handlePrevious}>Previous</button>
         </div>
         <div className='custom-btn ml-48'>
-          <button>Next</button>
+          <button onClick={handleNext}>Next</button>
         </div>
  
       </div>
