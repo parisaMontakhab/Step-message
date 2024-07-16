36fd4470fc1f34bc82336b167c81cd051e138bc4
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-16 10:52:09 +0200

    refactor: changed style and creat personal style

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -12,16 +12,16 @@ function App() {
     <div className='text-center w-2/4 mx-auto bg-slate-200 rounded-2xl mt-28 shadow-md'>
       <div className='grid grid-rows-3 grid-cols-3 gap-4 p-5'>
         <div className='active ml-32'>1</div>
-        <div className=''>2</div>
-        <div className=''>3</div>
+        <div className=' not-active ml-28'>2</div>
+        <div className='not-active ml-20'>3</div>
         <div className=' col-start-1  col-end-7 '>
-          <p className='text-lg font-bold text-indigo-800'> step {step } : "{message[step-1]}"</p>
+          <p className='text-lg font-bold text-indigo-800 mt-5 mb-5'> step {step } : "{message[step-1]}"</p>
         </div>
         <div className='custom-btn ml-80'>
-          <button>Next</button>
+          <button>Previous</button>
         </div>
         <div className='custom-btn ml-48'>
-          <button>Previous</button>
+          <button>Next</button>
         </div>
  
       </div>
