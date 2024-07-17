b2c70b4cf2fd7fb54a91076ca1ae8e2147949ad9
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-17 17:20:53 +0200

    refactor: changed the methode for solving challeneg about close and open modal

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -18,14 +18,15 @@ function handleNext (){
  
 function handleCloseOpen (){
   setIsOpen(!isOpen);
-  if( isOpen){
-  const uniqe = document.getElementById('uniqe');
-  uniqe.style.display= "none"
-  }
-  if(!isOpen){
-    const uniqe = document.getElementById('uniqe');
-    uniqe.style.display ="block"
-  }
+  // this metode is one of the sulotions:
+  // if( isOpen){
+  // const uniqe = document.getElementById('uniqe');
+  // uniqe.style.display= "none"
+  // }
+  // if(!isOpen){
+  //   const uniqe = document.getElementById('uniqe');
+  //   uniqe.style.display ="block"
+  // }
 }
 
 
@@ -34,24 +35,30 @@ function handleCloseOpen (){
       <div className='close-btn'>
         <button onClick={handleCloseOpen}>{isOpen ? "close" : "open"}</button>
       </div>
-      <div id='uniqe' className='step-father'>
-      <div className='grid grid-rows-3 grid-cols-3 gap-4 p-5'>
-        <div className={`${step >= 1 ? "active" :"not-active"} ml-32`}>1</div>
-        <div className={` ${step >= 2 ? "active" :"not-active"}  ml-28`}>2</div>
-        <div className={`${step >= 3 ? "active" :"not-active"} ml-20`}>3</div>
-        <div className=' col-start-1  col-end-7 '>
-          <p className='text-lg font-bold text-indigo-800 mt-5 mb-5'> step {step } : "{message[step-1]}"</p>
+      { //this is another methode to solve this challenge//
+        isOpen && (
+          <div id='uniqe' className='step-father'>
+          <div className='grid grid-rows-3 grid-cols-3 gap-4 p-5'>
+            <div className={`${step >= 1 ? "active" :"not-active"} ml-32`}>1</div>
+            <div className={` ${step >= 2 ? "active" :"not-active"}  ml-28`}>2</div>
+            <div className={`${step >= 3 ? "active" :"not-active"} ml-20`}>3</div>
+            <div className=' col-start-1  col-end-7 '>
+              <p className='text-lg font-bold text-indigo-800 mt-5 mb-5'> step {step } : "{message[step-1]}"</p>
+            </div>
+            <div className='custom-btn ml-80'>
+              <button onClick ={handlePrevious}>Previous</button>
+            </div>
+            <div className='custom-btn ml-48'>
+              <button onClick={handleNext}>Next</button>
+            </div>
+     
+          </div>
+        
         </div>
-        <div className='custom-btn ml-80'>
-          <button onClick ={handlePrevious}>Previous</button>
-        </div>
-        <div className='custom-btn ml-48'>
-          <button onClick={handleNext}>Next</button>
-        </div>
- 
-      </div>
-    
-    </div>
+
+        )
+      }
+     
     </div>
     
   )
