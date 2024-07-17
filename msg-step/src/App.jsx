e5802050a1e1b5d20ec2e154afe0431736d74df7
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-17 17:15:23 +0200

    feat: add btn about clos and open the dive about step message

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -7,6 +7,7 @@ const message =[
 
 function App() {
   const [step,setStep ] = useState(1)
+  const [isOpen,setIsOpen] = useState(true);
   
   function handlePrevious (){
    if(step >1) setStep(step-1)
@@ -14,9 +15,26 @@ function App() {
 function handleNext (){
  if(step <3) setStep(step+1)
 }
-  
+ 
+function handleCloseOpen (){
+  setIsOpen(!isOpen);
+  if( isOpen){
+  const uniqe = document.getElementById('uniqe');
+  uniqe.style.display= "none"
+  }
+  if(!isOpen){
+    const uniqe = document.getElementById('uniqe');
+    uniqe.style.display ="block"
+  }
+}
+
+
   return (
-    <div className='text-center w-2/4 mx-auto bg-slate-200 rounded-2xl mt-28 shadow-md'>
+    <div>
+      <div className='close-btn'>
+        <button onClick={handleCloseOpen}>{isOpen ? "close" : "open"}</button>
+      </div>
+      <div id='uniqe' className='step-father'>
       <div className='grid grid-rows-3 grid-cols-3 gap-4 p-5'>
         <div className={`${step >= 1 ? "active" :"not-active"} ml-32`}>1</div>
         <div className={` ${step >= 2 ? "active" :"not-active"}  ml-28`}>2</div>
@@ -34,6 +52,8 @@ function handleNext (){
       </div>
     
     </div>
+    </div>
+    
   )
 }
 
