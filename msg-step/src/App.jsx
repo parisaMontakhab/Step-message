8e7f938f53e8279d842c32c95971cc5b4b8a9ed5
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-18 17:53:55 +0200

    feat: add handleclick functions

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -1,108 +1,139 @@
-import React, { useState } from 'react';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-
-const message =[
-  "Learning React","Apply for job","Invest your new income"
-]
+const message = ["Learning React", "Apply for job", "Invest your new income"];
 
 function App() {
-
   //extract Step component for showing this : each component has own state and they work seprat//
-  
-  return(
+
+  return (
     <div>
-    
-      <ChallengeOne/>
+      <ChallengeOne />
     </div>
-  )
-
+  );
 }
 
-export default App
+export default App;
 
+function Step() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
-function Step(){
-  const [step,setStep ] = useState(1)
-  const [isOpen,setIsOpen] = useState(true);
-  
-  function handlePrevious (){
+  function handlePrevious() {
     //allways use the calback function for updating setState//
-   if(step >1) setStep((s)=> s-1)
+    if (step > 1) setStep((s) => s - 1);
   }
-function handleNext (){
- if(step <3) setStep((s)=> s+1)
-}
- 
-function handleCloseOpen (){
-  setIsOpen(!isOpen);
-  // this metode is one of the sulotions:
-  // if( isOpen){
-  // const uniqe = document.getElementById('uniqe');
-  // uniqe.style.display= "none"
-  // }
-  // if(!isOpen){
-  //   const uniqe = document.getElementById('uniqe');
-  //   uniqe.style.display ="block"
-  // }
-}
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
 
-return (
-  <div>
-    <div className='close-btn'>
-      <button onClick={handleCloseOpen}>{isOpen ? "close" : "open"}</button>
+  function handleCloseOpen() {
+    setIsOpen(!isOpen);
+    // this metode is one of the sulotions:
+    // if( isOpen){
+    // const uniqe = document.getElementById('uniqe');
+    // uniqe.style.display= "none"
+    // }
+    // if(!isOpen){
+    //   const uniqe = document.getElementById('uniqe');
+    //   uniqe.style.display ="block"
+    // }
+  }
+
+  return (
+    <div>
+      <div className="close-btn">
+        <button onClick={handleCloseOpen}>{isOpen ? "close" : "open"}</button>
+      </div>
+      {
+        //this is another methode to solve this challenge//
+        isOpen && (
+          <div id="uniqe" className="step-father">
+            <div className="grid grid-rows-3 grid-cols-3 gap-4 p-5">
+              <div className={`${step >= 1 ? "active" : "not-active"} ml-32`}>
+                1
+              </div>
+              <div className={` ${step >= 2 ? "active" : "not-active"}  ml-28`}>
+                2
+              </div>
+              <div className={`${step >= 3 ? "active" : "not-active"} ml-20`}>
+                3
+              </div>
+              <div className=" col-start-1  col-end-7 ">
+                <p className="text-lg font-bold text-indigo-800 mt-5 mb-5">
+                  {" "}
+                  step {step} : "{message[step - 1]}"
+                </p>
+              </div>
+              <div className="custom-btn ml-80">
+                <button onClick={handlePrevious}>Previous</button>
+              </div>
+              <div className="custom-btn ml-48">
+                <button onClick={handleNext}>Next</button>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </div>
-    { //this is another methode to solve this challenge//
-      isOpen && (
-        <div id='uniqe' className='step-father'>
-        <div className='grid grid-rows-3 grid-cols-3 gap-4 p-5'>
-          <div className={`${step >= 1 ? "active" :"not-active"} ml-32`}>1</div>
-          <div className={` ${step >= 2 ? "active" :"not-active"}  ml-28`}>2</div>
-          <div className={`${step >= 3 ? "active" :"not-active"} ml-20`}>3</div>
-          <div className=' col-start-1  col-end-7 '>
-            <p className='text-lg font-bold text-indigo-800 mt-5 mb-5'> step {step } : "{message[step-1]}"</p>
-          </div>
-          <div className='custom-btn ml-80'>
-            <button onClick ={handlePrevious}>Previous</button>
-          </div>
-          <div className='custom-btn ml-48'>
-            <button onClick={handleNext}>Next</button>
-          </div>
-   
-        </div>
-      
-      </div>
-
-      )
-    }
-   
-  </div>
-  
-)
-
+  );
 }
 
-  function ChallengeOne (){
- const day = new Date().toDateString();
- const[step,setStep] = useState(0);
- const [count,setCount] = useState(0);
- 
+function ChallengeOne() {
+  const day = new Date().toDateString();
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
 
+  function hanldlePlusStep() {
+    setStep((s) => s + 1);
+  }
+  function handleMinesStep() {
+    setStep((s) => s - 1);
+  }
 
+  function handlePlusCount() {
+    setCount((c) => c + 1);
+  }
 
-  return(
-      <div className="text-center mt-10">
-          <div className="grid grid-cols-3 w-48 mx-auto mb-5">
-              <button className="bg-slate-300 rounded-full font-bold text-2xl ">-</button>
-              <p>step :{step} </p>
-              <button className="bg-slate-300 rounded-full font-bold text-2xl "> +</button>
-          </div>
-          <div className="grid grid-cols-3 w-48 mx-auto mb-5">
-          <button className="bg-slate-300 rounded-full font-bold text-2xl ">-</button>
-              <p>count :0 </p>
-              <button className="bg-slate-300 rounded-full font-bold text-2xl "> +</button>
-          </div>
-          <div>today is {day}</div>
+  function handleMinesCount() {
+    setCount((c) => c - 1);
+  }
+
+  return (
+    <div className="text-center mt-10">
+      <div className="grid grid-cols-3 w-48 mx-auto mb-5">
+        <button
+          onClick={handleMinesStep}
+          className="bg-slate-300 rounded-full font-bold text-2xl "
+        >
+          -
+        </button>
+        <p>step :{step} </p>
+        <button
+          onClick={hanldlePlusStep}
+          className="bg-slate-300 rounded-full font-bold text-2xl "
+        >
+          {" "}
+          +
+        </button>
       </div>
-  )
+      <div className="grid grid-cols-3 w-48 mx-auto mb-5">
+        <button
+          onClick={handleMinesCount}
+          className="bg-slate-300 rounded-full font-bold text-2xl "
+        >
+          -
+        </button>
+        <p>count :{count}</p>
+        <button
+          onClick={handlePlusCount}
+          className="bg-slate-300 rounded-full font-bold text-2xl "
+        >
+          {" "}
+          +
+        </button>
+      </div>
+      <div>{count} days from today : {day}</div>
+    </div>
+  );
 }
