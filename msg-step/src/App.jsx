fcb2899ce874e1b9dc8626082625587fc8e3625f
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-08-06 10:53:19 +0200

    feat: coded the first structure of challeng

--- msg-step/src/App.jsx
+++ msg-step/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Step from "./components/Challenge1";
+import ChallengeTwo from "./components/Challenge2";
 
 
 
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <div>
-      <Step/>
+     <ChallengeTwo/>
     </div>
   );
 }
--- msg-step/src/components/Challenge2.jsx
+++ msg-step/src/components/Challenge2.jsx
@@ -2,5 +2,25 @@
 
 
 export default function ChallengeTwo (){
-    
+    return(
+        <div className="text-center">
+
+            <div className="mb-4 mt-2">
+                <h2>How much was the bill?</h2>
+                <input type="text" placeholder="0" className="bg-slate-200 p-2 text-center rounded-md"/>
+            </div>
+             
+            <div className="mb-4 mt-2">
+                <h2>How did you like the services?</h2>
+                <input type="text" placeholder="Dissatisfied(0%)" className="bg-slate-200 p-2 text-center rounded-md"/>
+            </div>
+
+             
+            <div className="mb-4 mt-2">
+                <h2>How did your friend like the services?</h2>
+                <input type="text" placeholder="Dissatisfied(0%)" className="bg-slate-200 p-2 text-center rounded-md"/>
+            </div>
+            
+        </div>
+    )
 }
