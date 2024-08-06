a9453f622dd4a8fdbc32a2821df5523685acfd12
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-08-06 17:57:32 +0200

    refactor: extract PercentageTip component and use children props and also lifting state up

--- msg-step/src/components/Challenge2.jsx
+++ msg-step/src/components/Challenge2.jsx
@@ -2,33 +2,21 @@ import { useState } from "react";
 
 export default function ChallengeTwo() {
   const [bill, setBill] = useState('');
+  const [tipOne, setTipOne] = useState(0);
+  const [tipTwo, setTipTwo] = useState(0);
 
 
   return (
     <div className="text-center">
+
      <BillInput bill={bill} setBill={setBill}/>
-      <div className="mb-4 mt-2">
-        <label>How did you like the services?</label>
-        <select className="bg-slate-200 p-2 text-center rounded-md mx-8">
-            <option>Dissatisfied (0%)</option>
-            <option>It was okay (5%)</option>
-            <option>It was good (10%)</option>
-            <option>Absolutely amazing (20%)</option>
-        </select>
-       
-      </div>
 
-      <div className="mb-4 mt-2">
-      <label>How did your friend like the services?</label>
-        <select className="bg-slate-200 p-2 text-center rounded-md mx-8">
-            <option>Dissatisfied (0%)</option>
-            <option>It was okay (5%)</option>
-            <option>It was good (10%)</option>
-            <option>Absolutely amazing (20%)</option>
-        </select>
+    <PercentageTip tip={tipOne} setTip={setTipOne}> How did you like the services?</PercentageTip>
+     
+    <PercentageTip tip={tipTwo} setTip={setTipTwo}> How did your friend  like the services?</PercentageTip>  
+     
         
-      </div>
-
+  
       {bill > 0 ? (
         <div className="mt-8">
           <p className="font-bold">
@@ -58,4 +46,20 @@ function BillInput({setBill,bill}){
   </div>
 
   )
+}
+
+function PercentageTip ({children,tip,setTip}){
+  return(
+    <div className="mb-4 mt-2">
+    <label>{children}</label>
+    <select className="bg-slate-200 p-2 text-center rounded-md mx-8" value={tip} onChange={(e)=>setTip(e.target.value)}>
+        <option value='0'>Dissatisfied (0%)</option>
+        <option value='5'>It was okay (5%)</option>
+        <option value='10'>It was good (10%)</option>
+        <option value='20'>Absolutely amazing (20%)</option>
+    </select>
+   
+  </div>
+  )
+
 }
