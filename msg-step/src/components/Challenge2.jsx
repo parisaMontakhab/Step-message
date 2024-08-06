fd95d31cc56430363157708fe287ffb14dd1d8d1
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-08-06 11:51:13 +0200

    feat: add select tag

--- msg-step/src/components/Challenge2.jsx
+++ msg-step/src/components/Challenge2.jsx
@@ -16,25 +16,30 @@ export default function ChallengeTwo() {
       </div>
 
       <div className="mb-4 mt-2">
-        <h2>How did you like the services?</h2>
-        <input
-          type="text"
-          placeholder="Dissatisfied(0%)"
-          className="bg-slate-200 p-2 text-center rounded-md"
-        />
+        <label>How did you like the services?</label>
+        <select className="bg-slate-200 p-2 text-center rounded-md mx-8">
+            <option>Dissatisfied (0%)</option>
+            <option>It was okay (5%)</option>
+            <option>It was good (10%)</option>
+            <option>Absolutely amazing (20%)</option>
+        </select>
+       
       </div>
 
       <div className="mb-4 mt-2">
-        <h2>How did your friend like the services?</h2>
-        <input
-          type="text"
-          placeholder="Dissatisfied(0%)"
-          className="bg-slate-200 p-2 text-center rounded-md"
-        />
+      <label>How did your friend like the services?</label>
+        <select className="bg-slate-200 p-2 text-center rounded-md mx-8">
+            <option>Dissatisfied (0%)</option>
+            <option>It was okay (5%)</option>
+            <option>It was good (10%)</option>
+            <option>Absolutely amazing (20%)</option>
+        </select>
+        
       </div>
+
       {bill > 0 ? (
         <div className="mt-8">
-          <p>
+          <p className="font-bold">
             you pay ${bill} (${bill} + $0 tip)
           </p>
           <button className="bg-slate-200 p-2 rounded-lg mt-4" onClick={()=>setBill(0)}>Reset</button>
