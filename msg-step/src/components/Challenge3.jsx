76472c6f14262cf35d42bd495f6988f0acd9fe04
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-08-13 11:17:20 +0200

    feat: add basic structure of component

--- msg-step/src/components/Challenge3.jsx
+++ msg-step/src/components/Challenge3.jsx
@@ -2,7 +2,14 @@
 
 export default function ChallengeTree (){
     return(
-        <>
-        hello</>
+        <div >
+            <div className="collapes-dive">
+                <p className="collapes-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta consequuntur eligendi modi fuga aut eius doloremque unde fugit odio autem numquam provident possimus, repudiandae vero suscipit? Assumenda tempore commodi fugiat?
+                <p className="collapes-show">show more</p>
+                </p>
+                
+            </div>
+            
+        </div>
     )
 }
