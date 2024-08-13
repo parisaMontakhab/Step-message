aef25d15e2c139ea10ddd99e8d537f96082f46f1
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-08-13 13:41:04 +0200

    refactor: changed methode of passing props and also changed the location of props

--- msg-step/src/components/Challenge3.jsx
+++ msg-step/src/components/Challenge3.jsx
@@ -1,41 +1,22 @@
 import { useState } from "react";
 
 export default function ChallengeTree() {
-    const [showOne,setShowOne] = useState(false);
-    const [showTwo,setShowTwo] = useState(false);
-    const [showTree,setShowTree] = useState(true);
+   
     
-
-
   return (
     <div>
-      <TextCard>
-        <p className={!showOne ? 'bg-red-200  p-2  text-nowrap overflow-hidden truncate' : 'bg-red-200  p-2'}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          consequuntur eligendi modi fuga aut eius doloremque unde fugit odio
-          autem numquam provident possimus, repudiandae vero suscipit? Assumenda
-          tempore commodi fugiat?
-          <p className="text-red-700 font-bold cursor-pointer" onClick={()=>setShowOne(!showOne)}>
-            {showOne ? 'Hide them' : 'Show more'}
-          </p>
-        </p>
-      </TextCard>
-
-      <TextCard>
-        <p className={!showTwo ? 'bg-red-200  p-2  text-nowrap overflow-hidden truncate' : 'bg-red-200  p-2'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ea dolorem repellendus corrupti perspiciatis reiciendis debitis iure, illum assumenda ipsum eum, minima aliquid sequi quisquam doloribus temporibus. Magnam, ab dolor
-            <p className="text-red-700 font-bold cursor-pointer" onClick={()=>setShowTwo(!showTwo)} >{showTwo ? 'Hide them' : 'Show more'}</p>
-        </p>
-      </TextCard>
-
-      <TextCard>
-        <p className={!showTree ? 'bg-red-200  p-2  text-nowrap overflow-hidden truncate' : 'bg-red-200  p-2'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ea dolorem repellendus corrupti perspiciatis reiciendis debitis iure, illum assumenda ipsum eum, minima aliquid sequi quisquam doloribus temporibus. Magnam, ab dolor
-            <p className="text-red-700 font-bold cursor-pointer" onClick={()=>setShowTree(!showTree)} >{showTree ? 'Hide them' : 'Show more'}</p>
-        </p>
-      </TextCard>
-
-
-     
       
+      <TextCard box='box' textExpen ='text-expend' textNotExpend ='text-notexpend' btn='btn' >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid ipsum eveniet, ea quod cumque reiciendis tempora voluptatem quae autem amet iusto alias totam perspiciatis dolores aspernatur maxime optio, odit quibusdam!
+      </TextCard>
+
+      <TextCard box='box' textExpen ='text-expend' textNotExpend ='text-notexpend' btn='btn' expended={true} >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio pariatur accusamus voluptatibus blanditiis, dolores dolor ex numquam perferendis sit voluptatum recusandae asperiores laudantium quasi sed? Non dolor temporibus eaque saepe.
+      </TextCard>
+
+      <TextCard box='box' textExpen ='text-expend' textNotExpend ='text-notexpend' btn='btn'  >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio pariatur accusamus voluptatibus blanditiis, dolores dolor ex numquam perferendis sit voluptatum recusandae asperiores laudantium quasi sed? Non dolor temporibus eaque saepe.
+      </TextCard>
      
     </div>
   );
@@ -44,6 +25,11 @@ export default function ChallengeTree() {
 
 
 
-function TextCard({ children }) {
-  return <div className="bg-red-300 w-3/5 p-4 mx-auto mt-10">{children}</div>;
+function TextCard({ children,box, textExpen,textNotExpend,btn,expended=false}) {
+     const [isExpended,setIsExpended] = useState(expended)
+
+  return <div className={box}>
+    <p className={isExpended ? textNotExpend : textExpen  }>{ children}</p>
+    <p className={btn} onClick={()=>setIsExpended(!isExpended)}>{isExpended ? 'show less' : 'show more'}</p>
+  </div>;
 }
