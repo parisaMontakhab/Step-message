662c68f7a9b5c8f2051dbcc707cbaa35fb407d12
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-08-13 12:05:37 +0200

    feat: import for two times textCard component with difrent state

--- msg-step/src/components/Challenge3.jsx
+++ msg-step/src/components/Challenge3.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 export default function ChallengeTree() {
     const [showOne,setShowOne] = useState(false);
+    const [showTwo,setShowTwo] = useState(false);
+    const [showTree,setShowTree] = useState(true);
+    
 
 
   return (
@@ -15,6 +18,18 @@ export default function ChallengeTree() {
           <p className="text-red-700 font-bold cursor-pointer" onClick={()=>setShowOne(!showOne)}>
             {showOne ? 'Hide them' : 'Show more'}
           </p>
+        </p>
+      </TextCard>
+
+      <TextCard>
+        <p className={!showTwo ? 'bg-red-200  p-2  text-nowrap overflow-hidden truncate' : 'bg-red-200  p-2'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ea dolorem repellendus corrupti perspiciatis reiciendis debitis iure, illum assumenda ipsum eum, minima aliquid sequi quisquam doloribus temporibus. Magnam, ab dolor
+            <p className="text-red-700 font-bold cursor-pointer" onClick={()=>setShowTwo(!showTwo)} >{showTwo ? 'Hide them' : 'Show more'}</p>
+        </p>
+      </TextCard>
+
+      <TextCard>
+        <p className={!showTree ? 'bg-red-200  p-2  text-nowrap overflow-hidden truncate' : 'bg-red-200  p-2'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ea dolorem repellendus corrupti perspiciatis reiciendis debitis iure, illum assumenda ipsum eum, minima aliquid sequi quisquam doloribus temporibus. Magnam, ab dolor
+            <p className="text-red-700 font-bold cursor-pointer" onClick={()=>setShowTree(!showTree)} >{showTree ? 'Hide them' : 'Show more'}</p>
         </p>
       </TextCard>
 
